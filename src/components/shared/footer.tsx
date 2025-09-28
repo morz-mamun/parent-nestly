b407df132b1c4acee4beae75d540797fd8c85b49
mamun <morzmamun@gmail.com>
2025-09-28 06:00:18 +0600

    feat: add social link in footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
 
   return (
     <footer className="relative max-w-screen mx-auto bg-secondary/5">
-      <div className="mx-auto max-w-screen-xl">
+      <div className="mx-auto max-w-screen-2xl">
         {/* footer content */}
         <div className="grid grid-cols-2 py-8 md:grid-cols-3 md:py-12 lg:grid-cols-4 border-b border-b-muted-foreground/10">
           {/* about us and contact us */}
@@ -106,29 +106,40 @@ export default function Footer() {
           <NewsletterForm />
         </div>
         {/* back to top button for large screens */}
-        <div className="absolute right-2 bottom-4 hidden lg:block">
+        <div className="absolute right-2 bottom-72 hidden lg:block">
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className="flex cursor-pointer items-center gap-2 rounded-full bg-gradient-to-b from-primary to-secondary px-4 py-3 text-white shadow-lg transition-colors hover:scale-105 hover:bg-gradient-to-b hover:from-secondary hover:to-primary duration-300 dark:shadow-lg"
+            className="flex cursor-pointer items-center gap-2 rounded-2xl bg-gradient-to-b from-primary to-secondary px-4 py-4 text-white shadow-lg transition-colors hover:scale-105 hover:bg-gradient-to-b hover:from-secondary hover:to-primary duration-300 dark:shadow-lg"
           >
             <ArrowUpFromLine size={24} className="animate-bounce text-white" />
-            Back to Top
+            {/* Back to Top */}
           </button>
         </div>
         {/* back to top button for medium and small screens */}
         <div className="flex items-center justify-center lg:hidden">
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className="flex cursor-pointer items-center gap-2 rounded-full bg-gradient-to-b from-[#0908C3] to-[#0C34E9] px-4 py-3 text-white shadow-lg transition-colors hover:scale-105 hover:bg-gradient-to-b hover:from-[#000EAC] hover:to-[#00163C] dark:shadow-lg"
+            className="flex cursor-pointer items-center gap-2 rounded-2xl bg-gradient-to-b from-[#0908C3] to-[#0C34E9] px-4 py-4 text-white shadow-lg transition-colors hover:scale-105 hover:bg-gradient-to-b hover:from-[#000EAC] hover:to-[#00163C] dark:shadow-lg"
           >
             <ArrowUpFromLine size={24} className="animate-bounce text-white" />
-            Back to Top
+            {/* Back to Top */}
           </button>
         </div>
       </div>
       {/* copyright */}
-      <div className="py-5 text-center text-zinc-500">
+      <div className="max-w-screen-2xl mx-auto flex justify-between py-5 text-center text-zinc-500">
         <p className="">© {currentYear} ParentNestly. All rights reserved.</p>
+        <div>
+          <ul className="flex justify-center space-x-4">
+            {Object.entries(socialMediaData).map(([platform, url]) => (
+              <li key={platform}>
+                <Link href={url} target="_blank" rel="noopener noreferrer">
+                  <Icon icon={`mdi:${platform}`} className="h-5 w-5" />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
