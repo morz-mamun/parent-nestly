eef17f1065cbf9cf9d46e5d1867eab3f3828cd8f
morshed <morzmamun@gmail.com>
2025-11-06 21:36:36 +0600

    fix: fixed env url

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -37,8 +37,8 @@ export default function AdminLoginPage() {
     setIsLoading(true);
 
     setTimeout(() => {
-      const validUsername = process.env.NEXT_PUBLIC_ADMIN_USERNAME;
-      const validPassword = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+      const validUsername = process.env.ADMIN_USERNAME;
+      const validPassword = process.env.ADMIN_PASSWORD;
 
       if (data.username === validUsername && data.password === validPassword) {
         localStorage.setItem("adminAuth", "true");
