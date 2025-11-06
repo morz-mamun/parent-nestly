1bbbf012b4979720c74e15a06d3334094324ad42
morshed <morzmamun@gmail.com>
2025-11-06 21:37:27 +0600

    fix: fixed env url

--- src/app/admin/dashboard/blogs/page.tsx
+++ src/app/admin/dashboard/blogs/page.tsx
@@ -127,7 +127,7 @@ export default function Blogs() {
     formData.append("image", file);
 
     const res = await fetch(
-      `https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`,
+      `https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`,
       { method: "POST", body: formData },
     );
     const data = await res.json();
